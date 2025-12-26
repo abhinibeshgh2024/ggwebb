134721ef02a8bc88710788c718eacb1e169edff0
abhinibeshgh2024 <153261309+abhinibeshgh2024@users.noreply.github.com>
2025-12-26 13:35:30 +0530

    Create unigallery.js

---
+++ unigallery.js
@@ -0,0 +1,48 @@
+const imageData = {
+    devotion: [
+        "https://picsum.photos/id/1011/800/1200",
+        "https://picsum.photos/id/1025/1200/800",
+        "https://picsum.photos/id/1040/900/1300"
+    ],
+    nature: [
+        "https://picsum.photos/id/1056/1200/800",
+        "https://picsum.photos/id/1069/800/1200"
+    ],
+    environment: [
+        "https://picsum.photos/id/1074/900/1200",
+        "https://picsum.photos/id/1084/1200/800"
+    ],
+    monuments: [
+        "https://picsum.photos/id/1081/800/1200",
+        "https://picsum.photos/id/1031/1200/800"
+    ]
+};
+
+function loadGallery(category) {
+    const gallery = document.getElementById("gallery");
+    const title = document.getElementById("sectionTitle");
+
+    gallery.innerHTML = "";
+    title.innerText = category.charAt(0).toUpperCase() + category.slice(1);
+
+    imageData[category].forEach(src => {
+        const card = document.createElement("div");
+        card.className = "gallery-card";
+
+        card.innerHTML = `
+            <img src="${src}" alt="image">
+            <div class="download-bar">
+                <a href="${src}" download>PNG</a>
+                <a href="${src}" download>JPG</a>
+                <a href="${src}" download>PDF</a>
+            </div>
+        `;
+
+        gallery.appendChild(card);
+    });
+
+    window.scrollTo({ top: 120, behavior: "smooth" });
+}
+
+// Default load
+loadGallery("devotion");
